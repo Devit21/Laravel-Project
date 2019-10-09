fc1e60f6620165423636d764f38d1e6708ca79e9
Devit21 <rossovandevit@gmail.com>
2019-10-09 16:01:36 +0700

    Migrate tbl_Products

---
+++ Coffee-Shop/app/Tbl_product.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Tbl_product extends Model
+{
+    //
+}
---
+++ Coffee-Shop/database/migrations/2019_10_09_084748_create_tbl_products_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTblProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tbl_products', function (Blueprint $table) {
+            $table->increments('p_id');
+            $table->string('p_name','255');
+            $table->string('price','255');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tbl_products');
+    }
+}
